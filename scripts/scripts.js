e718b60565c772a5b9b23825d5492004b03147df
arakhuzia <142080940+arakhuziaBKH@users.noreply.github.com>
2024-02-13 12:12:57 +0330

    a

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-const text = $(".text");
-const button = $(".button");
-
-button.addEventListener("click", ()=>{
-    navigator.clipboard.writeText("hi");
-});
-
 
 });
