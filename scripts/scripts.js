f3cc22c7afcdb12404d3d61b9a19c2e3fe13d074
arakhuzia <142080940+arakhuziaBKH@users.noreply.github.com>
2024-02-16 19:20:49 +0330

    a

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
 
+    $("#contactMe").on("click", function(e){
+        Notification.requestPermission().then(permissions => {
+            if (permission === "granted"){
+                new Notification($("#contactMe").text());
+            }
+        });
+    });
+
 });
