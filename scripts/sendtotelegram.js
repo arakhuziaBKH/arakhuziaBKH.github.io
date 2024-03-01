45030d6e34d9aa52e22df357c5e8f030c0b28f88
arakhuzia <142080940+arakhuziaBKH@users.noreply.github.com>
2024-03-01 23:07:14 +0330

    a

--- scripts/sendtotelegram.js
+++ scripts/sendtotelegram.js
@@ -9,11 +9,18 @@ let tg = {
  * @param {String} text - The text to send
  */
 function sendMessage(text) {
-    const url = `https://api.telegram.org/bot${tg.token}/sendMessage?chat_id=${tg.chat_id}&text=${text}`;
-    const xht = new XMLHttpRequest();
-    xht.open("GET", url);
-    xht.send();
+        
+    const url = `https://api.telegram.org/bot${tg.token}/sendMessage`;
+            const obj = {
+                    chat_id: tg.chat_id,
+                    text: text
+            };
+            const xht = new XMLHttpRequest();
+            xht.open("POST", url, true);
+            xht.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+            xht.send(JSON.stringify(obj));
 }
+
 
 const username = document.querySelector("#username");
 const email = document.querySelector("#email");
