78f94f0e03739f09b7fdc2f42c48d0eb4e78b026
arakhuzia <142080940+arakhuziaBKH@users.noreply.github.com>
2024-03-01 18:05:15 +0330

    a

--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -4,4 +4,9 @@ var sideMenuIcon = document.getElementsByClassName("sideMenuIcon");
 //selecting side menu div
 var sideMenuContainer = document.getElementById("sideMenu");
 
+sideMenuContainer.style.display = "none";
 
+//adding event listener to side menu icon
+sideMenuIcon.addEventListener("click", (e)=>{
+      sideMenuContainer.style.display = "flex";
+});
