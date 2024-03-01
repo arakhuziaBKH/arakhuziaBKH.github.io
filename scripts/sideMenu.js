3af3fc0bd699705bb9507f7cbe8dba4201a83307
arakhuzia <142080940+arakhuziaBKH@users.noreply.github.com>
2024-03-01 17:55:30 +0330

    a

---
+++ scripts/sideMenu.js
@@ -0,0 +1,7 @@
+//seclecting side menu icon
+var sideMenuIcon = document.getElementsByClassName("sideMenuIcon");
+
+//selecting side menu div
+var sideMenuContainer = document.getElementById("sideMenu");
+
+
