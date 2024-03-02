b0c954e762f85b925f76070904c1448273738ac8
arakhuzia <142080940+arakhuziaBKH@users.noreply.github.com>
2024-03-02 18:29:36 +0330

    a

--- scripts/sideMenu.js
+++ scripts/sideMenu.js
@@ -5,17 +5,21 @@ sideMenuContainer = document.getElementById("sideMenuContainer");
 sideMenuClose = document.getElementById("sideMenuClose");
 
 sideMenuContainer.style.display = "none";
+sideMenuContainer.style.width = "0";
 
 sideMenuIcon.addEventListener("click", (e) =>{
   if(sideMenuContainer.style.display == "none"){
     sideMenuContainer.style.display = "flex";
+    sideMenuContainer.style.width = "100dvw";
   }else{
     sideMenuContainer.style.display = "none"
+    sideMenuContainer.style.width = "0"
   }
 });
 
 sideMenuClose.addEventListener("click", (e) => {
   if (sideMenuContainer.style.display == "flex") {
     sideMenuContainer.style.display = "none";
+    sideMenuContainer.style.width = "0";
   }
 });
